dd8128bf2f1aeaa2dde201319804e91ec2ff7f40
avierusu <avierusu@gmail.com>
2024-06-14 19:02:10 -0500

    One-line output with multiple print statements

---
+++ C++/01_Intro/02-HelloWorld2.cpp
@@ -0,0 +1,8 @@
+// Print text on one line using multiple printf statements
+
+#include <iostream>
+
+int main(){
+    std::cout << "Welcome ";
+    std::cout << "to C++\n";
+}
---
+++ C/01_Intro/02-HelloWorld2.c
@@ -0,0 +1,8 @@
+// Print text on one line using multiple printf statements
+
+#include <stdio.h>
+
+int main(void){
+    printf("Welcome ");
+    printf("to C!\n");
+}
