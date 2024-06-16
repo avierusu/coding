ee27c0263f12022924ecebe805ee53357c613c4f
avierusu <avierusu@gmail.com>
2024-06-16 14:44:58 -0500

    Added account balance functionality to Account class

--- C++/03_ClassesAndObjects/01-AccountTest.cpp
+++ C++/03_ClassesAndObjects/01-AccountTest.cpp
@@ -8,9 +8,29 @@
 using namespace std;
 
 int main(){
-    Account acc1{"Jane Green"};
-    Account acc2{"John Blue"};
+    Account account1{"Jane Green", 50};
+    Account account2{"John Blue", -7};
 
-    cout << "account1 name is: " << acc1.getName() << endl;
-    cout << "account2 name is: " << acc2.getName() << endl;
+    // Display initial balances
+    cout << "account1: " << account1.getName() << " balance is $" << account1.getBalance();
+    cout << "\naccount2: " << account2.getName() << " balance is $" << account2.getBalance();
+
+    cout << "\n\nEnter deposit amount for account1: ";
+    int depositAmount;
+    cin >> depositAmount;
+    cout << "adding " << depositAmount << " to account1 balance";
+    account1.deposit(depositAmount);
+
+    // Display balances
+    cout << "\n\naccount1: " << account1.getName() << " balance is $" << account1.getBalance();
+    cout << "\naccount2: " << account2.getName() << " balance is $" << account2.getBalance();
+
+    cout << "\n\nEnter deposit amount for account2: ";
+    cin >> depositAmount;
+    cout << "adding " << depositAmount << " to account2 balance";
+    account2.deposit(depositAmount);
+
+    // Display balances
+    cout << "\n\naccount1: " << account1.getName() << " balance is $" << account1.getBalance();
+    cout << "\naccount2: " << account2.getName() << " balance is $" << account2.getBalance();
 }
--- C++/03_ClassesAndObjects/Account.h
+++ C++/03_ClassesAndObjects/Account.h
@@ -4,14 +4,35 @@
 
 class Account {
     public:
-        explicit Account(std::string accName) : name{accName}{}
+        // Account constructor
+        explicit Account(std::string accName, int initBalance) : name{accName}{
+            if(initBalance > 0){
+                balance = initBalance;
+            }
+        }
+
+        // Deposits a valid amount to the balance
+        void deposit(int depositAmount){
+            if(depositAmount > 0){
+                balance = balance + depositAmount;
+            }
+        }
+
+        // Return the account balance
+        int getBalance() const {
+            return balance;
+        }
+
+        // Set the name of the account
         void setName(std::string accName){
             name = accName;
         }
- 
+
+        // Return the name of the account
         std::string getName() const {
             return name;
         }
     private:
         std::string name;
+        int balance{0};
 };
