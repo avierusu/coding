8f9fc9e2413128ff6eaf0d99b246885ad26b2bb6
avierusu <avierusu@gmail.com>
2024-06-19 10:57:08 -0500

    Do...While iteration

---
+++ C/02_ControlStructures/10-DoWhileLoops.c
@@ -0,0 +1,11 @@
+// Using the do...while iteration statement
+
+#include <stdio.h>
+
+int main(void){
+    int counter = 1;
+
+    do {
+        printf("%d  ", counter);
+    } while (++counter <= 5);
+}
