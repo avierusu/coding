2fd5d35e7ff1d0e3f886f04086b534d8500325d0
avierusu <avierusu@gmail.com>
2024-06-20 15:07:57 -0500

    Roll dice 60 million times

--- C++/04_Functions/03-SixSidedDie.cpp
+++ C++/04_Functions/03-SixSidedDie.cpp
@@ -6,11 +6,44 @@
 using namespace std;
 
 int main(){
-    for(unsigned int counter{1}; counter <= 20; counter++){
-        cout << setw(10) << (1 + rand() % 6);
+    unsigned int freq1{0};
+    unsigned int freq2{0};
+    unsigned int freq3{0};
+    unsigned int freq4{0};
+    unsigned int freq5{0};
+    unsigned int freq6{0};
+    int face;
 
-        if(counter % 5 == 0){
-            cout << endl;
+    for(unsigned int roll{1}; roll <= 60000000; roll++){
+        face = 1 + rand() % 6;
+
+        switch(face){
+            case 1:
+                freq1++;
+                break;
+            case 2:
+                freq2++;
+                break;
+            case 3:
+                freq3++;
+                break;
+            case 4:
+                freq4++;
+                break;
+            case 5:
+                freq5++;
+                break;
+            case 6:
+                freq6++;
+                break;
         }
     }
+
+    cout << "Face" << setw(13) << "Frequency" << endl;
+    cout << "   1" << setw(13) << freq1
+       << "\n   2" << setw(13) << freq2
+       << "\n   3" << setw(13) << freq3
+       << "\n   4" << setw(13) << freq4
+       << "\n   5" << setw(13) << freq5
+       << "\n   6" << setw(13) << freq6 << endl;
 }
--- C/03_Functions/03-SixSidedDie.c
+++ C/03_Functions/03-SixSidedDie.c
@@ -4,9 +4,43 @@
 #include <stdlib.h>
 
 int main(void){
-    for(int i = 1; i <= 10; i++){
-        printf("%d  ", 1 + (rand() % 6));
+    int freq1 = 0;
+    int freq2 = 0;
+    int freq3 = 0;
+    int freq4 = 0;
+    int freq5 = 0;
+    int freq6 = 0;
+
+    for(int roll = 1; roll <= 60000000; roll++){
+        int face = 1 + rand() % 6;
+
+        switch(face){
+            case 1:
+                freq1++;
+                break;
+            case 2:
+                freq2++;
+                break;
+            case 3:
+                freq3++;
+                break;
+            case 4:
+                freq4++;
+                break;
+            case 5:
+                freq5++;
+                break;
+            case 6:
+                freq6++;
+                break;
+        }
     }
 
-    puts("");
+    printf("%s%13s\n", "Face", "Frequency");
+    printf("   1%13d\n", freq1);
+    printf("   2%13d\n", freq2);
+    printf("   3%13d\n", freq3);
+    printf("   4%13d\n", freq4);
+    printf("   5%13d\n", freq5);
+    printf("   6%13d\n", freq6);
 }
