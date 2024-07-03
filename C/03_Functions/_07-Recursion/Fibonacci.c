2beebf381d81916a24bf9f4ce745eeefa548d614
avierusu <avierusu@gmail.com>
2024-07-03 13:58:48 -0500

    Recursion - Fibonacci

---
+++ C/03_Functions/_07-Recursion/Fibonacci.c
@@ -0,0 +1,24 @@
+// Recursive function fibonacci
+
+#include <stdio.h>
+
+unsigned long long int fibonacci(int n);    // function prototype
+
+int main(void){
+    // calculate fibonacci values of 0 through 10
+    for(int number = 0; number <= 10; number++){
+        printf("Fibonacci(%d) = %llu\n", number, fibonacci(number));
+    }
+
+    printf("Fibonacci(20) = %llu\n", fibonacci(20));
+    printf("Fibonacci(30) = %llu\n", fibonacci(30));
+    printf("Fibonacci(40) = %llu\n", fibonacci(40));
+}
+
+unsigned long long int fibonacci(int n){
+    if(n == 0 || n == 1){
+        return n;                                       // base case
+    } else {
+        return fibonacci (n - 1) + fibonacci (n - 2);   // recursive step
+    }
+}
