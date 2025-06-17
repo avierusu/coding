83d66b7b289768a3ea842fe42c7708a28d2d0044
avierusu <avierusu@gmail.com>
2025-06-17 10:03:37 -0500

    Modify code

--- C/07_FileHandling/_02-ReadFromFile/ReadFromFile.c
+++ C/07_FileHandling/_02-ReadFromFile/ReadFromFile.c
@@ -4,29 +4,33 @@
 
 int main(){
     // Define the name of the file
-    const char fileName[] = "clients.txt";
+    char file[] = "clients.txt";
     // Define a file pointer and open the file
     FILE *filePtr;
-    filePtr = fopen(fileName, "r");
+    filePtr = fopen(file, "r");
     
     // exit the program if the file cannot be opened
     if (filePtr == NULL){
         printf("File could not be opened.\n");
-    } else {
-        // Read account number, name, and balance from file
-        int account;
-        char name[30];
-        double balance;
-
-        printf("%-10s%-13s%s\n", "Account", "Name", "Balance");
-        fscanf(filePtr, "%d%29s%lf", &account, name, &balance);
-
-        // While not end of file
-        while (!feof(filePtr)){
-            printf("%-10d%-13s%7.2f\n", account, name, balance);
-            fscanf(filePtr, "%d%29s%lf", &account, name, &balance);
-        }
+        return 1;
     }
 
+    // Create variables to store account number, name, and balance from file
+    int account;
+    char name[30];
+    double balance;
+
+    printf("%-10s%-13s%s\n", "Account", "Name", "Balance");
+    // Read and display account number, name, and balance from file
+    fscanf(filePtr, "%d%29s%lf", &account, name, &balance);
+
+    // While not end of file
+    while (!feof(filePtr)){
+        printf("%-10d%-13s%7.2f\n", account, name, balance);
+        fscanf(filePtr, "%d%29s%lf", &account, name, &balance);
+    }
+
+    fclose(file);
+    
     return 0;
 }
