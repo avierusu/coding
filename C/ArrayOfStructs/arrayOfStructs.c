db43866c7b75428494a1a234fb2f33b7be717820
avierusu <avierusu@gmail.com>
2025-10-15 22:36:25 -0500

    Store Data from File in the Correct Format

--- C/ArrayOfStructs/arrayOfStructs.c
+++ C/ArrayOfStructs/arrayOfStructs.c
@@ -40,6 +40,7 @@
 #define FIRST_NAME_LENGTH 10
 #define MIDDLE_NAME_LENGTH 10
 #define LAST_NAME_LENGTH 15
+#define FULL_NAME_LENGTH 40
 #define ACCT_LENGTH 7
 #define NUM_GRADES 10
 // There are actually 12 grades, but to search for a -1, count is increased by 1
@@ -51,16 +52,22 @@
 // Define and name a struct to store student records
 typedef struct record{
     int ID;
-    char firstName[FIRST_NAME_LENGTH + 1];
-    char middleName[MIDDLE_NAME_LENGTH + 1];
-    char lastName[LAST_NAME_LENGTH + 1];
+    char name[FULL_NAME_LENGTH+1];
     char account[ACCT_LENGTH + 1];
     float grades[NUM_GRADES];
     float avg;
 } Record;
 
-// Function prototypes
+/*****************************************************************************************************
+                                        FUNCTION PROTOTYPES
+*****************************************************************************************************/
+// For reading and storing data
 void readData(char *inFile, char *outFile, Record students[], int *numRecords, int *numGrades);
+void populateStruct(Record students[], int *numRecords, int *numGrades, char *line);
+char* skipSpace(char *line);
+void createName(char fullName[], char firstName[], char middleName[], char lastName[]);
+void createAccount(char account[], char first[], char middle[], char last[], char ID[]);
+float gradeAvg(Record student, int numGrades);
 
 
 int main(){
@@ -112,14 +119,182 @@ void readData(char *inFile, char *outFile, Record students[], int *numRecords, i
     It also creates an account ID for each student, and calculates the average of the students'
     grades
 *****************************************************************************************************/
-void populateStruct(Record students[], int *numRecords, int *numgrades, char *line){
+void populateStruct(Record students[], int *numRecords, int *numGrades, char *line){
+    // Create arrays to store the student's first, middle, and last names, as well as their
+    // full name, id, and account id
+    char lastName[LAST_NAME_LENGTH],
+         firstName[FIRST_NAME_LENGTH],
+         middleName[MIDDLE_NAME_LENGTH],
+         fullName[FULL_NAME_LENGTH],
+         studentId[ID_SIZE],
+         accountId[ACCT_LENGTH],
+         temp[30];
     
+    // Set grade amount to 0 and increment by 1 for each grade we store
+    *numGrades = 0;
+
+    while (*line != '\0'){
+        // if *line is pointing to a letter or a digit
+        if (isalnum(*line)){
+            // Read student ID as a string
+            sscanf(line, "%s", temp);
+            strcpy(studentId, temp);
+
+            // Use studentId to create a student record
+            students[*numRecords].ID = atoi(temp);
+
+            // Move "line" to the end of ID
+            line += strlen(temp);
+            line = skipSpace(line);
+
+            // Read last name
+            sscanf(line, "%s", temp);
+            strcpy(lastName, temp);
+            // Move "line" to the end of last name
+            line += strlen(temp);
+            line = skipSpace(line);
+
+            // Read first name
+            sscanf(line, "%s", temp);
+            strcpy(firstName, temp);
+            // Move "line" to the end of first name
+            line += strlen(temp);
+            line = skipSpace(line);
+
+            // Read middle name
+            sscanf(line, "%s", temp);
+            strcpy(middleName, temp);
+            // Move "line" to the end of middle name
+            line += strlen(temp);
+            line = skipSpace(line);
+
+
+            // Loop through all grades and store them
+            while (isdigit(*line)){
+                // Read the next grade
+                sscanf(line, "%s", temp);
+                // Add the grade to the current studet's record
+                students[*numRecords].grades[*numGrades] = atof(temp);
+
+                // Move "line" to the end of the grade
+                line += strlen(temp);
+                line = skipSpace(line);
+
+                // Increment grade count by 1
+                *numGrades++;
+            }
+
+
+            // Create a full name from first, middle, and last
+            // Set all characters to null so that when we combine
+            // first, middle, and last names, excess space after
+            // the full name will be null
+            for (int index = 0; index < FULL_NAME_LENGTH; index++){
+                fullName[index] = '\0';
+            }
+            createName(fullName, firstName, middleName, lastName);
+            // Store full name in the current student record
+            strcpy(students[*numRecords].name, fullName);
+
+            // Create account ID and store it
+            createAccount(accountId, firstName, middleName, lastName, studentId);
+            // Store account ID in the current student record
+            strcpy(students[*numRecords].account, accountId);
+
+            // Calculate the average for all the student's grades
+            students[*numRecords].avg = gradeAvg(students[*numRecords], *numGrades);
+        } else {
+            // If "line" is pointing to an empty space, move the pointer to the next
+            // element
+            line = skipSpace(line);
+        }
+    }
 }
 
-void writeBinaryFile(Record students[], int numRecords, FILE *filePtrBin){
 
+/*****************************************************************************************************
+    This function traverses the line character by character to skip all spaces and stop the pointer
+    at the next non-space character
+*****************************************************************************************************/
+char* skipSpace(char *line){
+    // Keep checking for empty spaces
+    while (isspace(*line)){
+        // Increment line pointer
+        line++;
+    }
+
+    return line;
 }
 
+/*****************************************************************************************************
+    This function creates a full name for the student, comprising of their first, middle, and
+    last name, with spaces in between each
+*****************************************************************************************************/
+void createName(char fullName[], char firstName[], char middleName[], char lastName[]){
+    // Add first name to full name
+    strcat(fullName, firstName);
+    // Add middle name to full name
+    strcat(fullName, " ");
+    strcat(fullName, middleName);
+    // Add last name to full name
+    strcat(fullName, " ");
+    strcat(fullName, lastName);
+}
+
+/*****************************************************************************************************
+    This function creates an account ID in the form "abc123", where "abc" are the student's first,
+    middle, and last initials respectively, and "1234" are the last four digits of the student's ID
+*****************************************************************************************************/
+void createAccount(char account[], char first[], char middle[], char last[], char ID[]){
+    sprintf(account, "%c%c%c%s", *first, *middle, *last, (ID+1));
+}
+
+/*****************************************************************************************************
+    This function calculates the average of all grades for each student record
+*****************************************************************************************************/
+float gradeAvg(Record student, int numGrades){
+    // Create float variables to store sum and average
+    float sum = 0.0, average = 0.0;
+
+    // Loop through each grade and add it to sum
+    for (int gradeIndex = 0; gradeIndex < numGrades; gradeIndex++){
+        // Add the current grade to the sum
+        sum += student.grades[gradeIndex];
+    }
+
+    // Validate against dividing by zero
+    if(numGrades != 0){
+        // Calculate average
+        average = sum / numGrades;
+    }
+
+    return average;
+}
+
+/*****************************************************************************************************
+    
+*****************************************************************************************************/
 void sortById(Record students[], int numRecords){
+
+}
+
+/*****************************************************************************************************
+    
+*****************************************************************************************************/
+void sortByGrades(Record students[], int numRecords){
+
+}
+
+/*****************************************************************************************************
+    
+*****************************************************************************************************/
+void sortByName(Record students[], int numRecords){
+
+}
+
+/*****************************************************************************************************
+    
+*****************************************************************************************************/
+void writeBinaryFile(Record students[], int numRecords, FILE *filePtrBin){
 
 }
